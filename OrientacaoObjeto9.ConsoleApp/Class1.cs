f7f1a20c0200d65c81433a829f357b9d9bd7112e
Vogeel <vitorvogel12@gmail.com>
2022-03-08 16:35:04 -0300

    Add project files.

---
+++ OrientacaoObjeto9.ConsoleApp/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjeto9.ConsoleApp
+{
+    internal class Aluno
+    {
+        public double n1, n2, n3, n4;
+        public double Media()
+        {
+            double ValorNota = (4 / ((1 / n1) + (1 / n2) + (1 / n3) + (1 / n4)));
+            return ValorNota;
+        }
+    }
+}
---
+++ OrientacaoObjeto9.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OrientacaoObjeto9.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno notas = new Aluno();
+            notas.n1 = 2;
+            notas.n2 = 6;
+            notas.n3 = 4;
+            notas.n4 = 5;
+
+            Console.WriteLine($"A média armonica do aluno foi de: {notas.Media():F2}");
+            Console.ReadKey();
+        }
+    }
+}
